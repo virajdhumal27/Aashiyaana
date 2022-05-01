7f302834e8ecc178aa3bc9c8cb8eebb0e580b79f
Viraj Dhumal <mevirajdhumal@gmail.com>
2022-05-01 21:34:46 +0530

    Added backend project packages
    Added required packages, all packages are empty.

---
+++ aashiyaana-backend/src/main/java/com/aashiyaana/aashiyaanabackend/AashiyaanaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aashiyaana.aashiyaanabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AashiyaanaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AashiyaanaBackendApplication.class, args);
+	}
+
+}
---
+++ aashiyaana-backend/src/test/java/com/aashiyaana/aashiyaanabackend/AashiyaanaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aashiyaana.aashiyaanabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AashiyaanaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
