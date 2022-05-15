9098b49a53b665a4ec424fda0f74e4ef1636e8e7
Viraj Dhumal <mevirajdhumal@gmail.com>
2022-05-15 19:04:21 +0530

    new initial commit

---
+++ aashiyaanabackend/src/main/java/com/aasihyaana/aashiyaanabackend/AashiyaanabackendApplication.java
@@ -0,0 +1,13 @@
+package com.aasihyaana.aashiyaanabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AashiyaanabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AashiyaanabackendApplication.class, args);
+	}
+
+}
